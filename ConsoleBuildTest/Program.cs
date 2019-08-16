e8ba047897abf8e513df71eb016c8772ead80ea3
pauline <paulineXing@outlook.com>
2019-08-16 16:46:55 +0800

    aa

---
+++ ConsoleBuildTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleBuildTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var obj = new { Name = "Tyler", Age = 28 };
+            string jonStr = JsonConvert.SerializeObject(obj);
+            Console.WriteLine(jonStr);
+            Console.ReadKey();
+        }
+    }
+}
