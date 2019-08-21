0ac56cf45dca856b46d4340735ef4e0f3235931a
xing.li <xing.li@dudesolutions.com>
2019-08-21 17:04:53 +0800

    change age

--- ConsoleBuildTest/Program.cs
+++ ConsoleBuildTest/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleBuildTest
     {
         static void Main(string[] args)
         {
-            var obj = new { Name = "Tyler", Age = 30 };
+            var obj = new { Name = "Tyler", Age = 38 };
             string jonStr = JsonConvert.SerializeObject(obj);
             Console.WriteLine(jonStr);
             Console.ReadKey();
